28170b3b91cd99c23a821629e4c389533836514b
nenyeonyema <chinenyeonyema1@gmail.com>
2024-01-07 16:49:24 -0800

    height of the tree

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,7 +8,7 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	if (tree == NULL)
+	if (!tree || (!tree->left && !tree->right))
 		return (0);
 
 	size_t left_height = binary_tree_height(tree->left);
